852ac8ad456d5a4956e425e50085d1ba9d6b4691
dongge <dongjiancheng2003@163.com>
2024-10-18 00:42:25 +0800

    Demo:复制文件夹

--- src/com/IO/Demo03/Test.java
+++ src/com/IO/Demo03/Test.java
@@ -3,6 +3,14 @@ package com.IO.Demo03;
 public class Test {
     public static void main(String[] args) {
         //加密和解密文件
+        //"C:\Users\dong\Desktop\原始文件.txt"
+
+        String original = "C:\\Users\\dong\\Desktop\\原始文件.txt";
+        String encrypt = "C:\\Users\\dong\\Desktop\\加密文件.txt";
+        String decrypt = "C:\\Users\\dong\\Desktop\\解密文件.txt";
+
+
+
 
     }
 }
